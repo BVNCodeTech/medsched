de343d4cac5f9fd92ddfb63edfc9922396611e2f
Aditya Gupta <divyashgupta2005@gmail.com>
2021-07-23 09:26:22 +0530

    Added more colors and elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-
+      colors: {
+        primary: {
+          // blue: "#4a49c9",
+          blue: {
+            light: "#4a49c9",
+            dark : "#66677a",
+            accent: "#d7d9f2"
+          },
+          green: {
+            light: "#c7ebdd",
+            dark: "#506b60",
+          },
+          red: {
+            accent: "#ffd6c4",
+            dark: "#9a7d6f"
+          },
+          yellow: {
+            accent: "#fff9e6",
+            dark: "#87700c",
+          }
+        },
+        
+      },
     },
   },
   variants: {
